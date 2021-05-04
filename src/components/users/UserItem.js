8bc123c25a89dd59e1378f24a465ba2919e7f9ae
Sanskruti Gunde <sanskruti.gunde@gmail.com>
2021-05-04 17:52:43 +0530

    ➕ Github user search

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,32 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 
-class UserItem extends Component {
-  state = {
-    id: "",
-    login: "omkarkirpan",
-    avatar_url: "https://avatars.githubusercontent.com/u/14964401?v=4",
-    html_url: "https://github.com/OmkarKirpan",
-  };
-
-  render() {
-    const { login, avatar_url, html_url } = this.state;
-    return (
-      <div className="card text-center">
-        <img
-          src={avatar_url}
-          alt=""
-          className="round-img"
-          style={{ width: "60px" }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a href={html_url} className="btn btn-dark btn-sm my-1">
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className="card text-center">
+      <img
+        src={avatar_url}
+        alt=""
+        className="round-img"
+        style={{ width: "60px" }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          target="_blank"
+          rel="noreferrer"
+          className="btn btn-dark btn-sm my-1"
+        >
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired,
+};
 
 export default UserItem;
